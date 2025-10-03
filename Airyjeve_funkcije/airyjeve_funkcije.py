90fdaa3b6abb366d1d58532b9d54bc48c606dac4
boringbug <paradiznik5@gmail.com>
2025-10-03 20:11:19 +0200

    added the airy funcitons

--- Airyjeve_funkcije/airyjeve_funkcije.py
+++ Airyjeve_funkcije/airyjeve_funkcije.py
@@ -1,1 +1,11 @@
-print("Hello world")
+import matplotlib.pyplot as plt
+import numpy as np
+import mpmath as mp
+from scipy.special import airy
+
+x = np.linspace(-20, 2, 1000)
+y1, _, y2, _ = airy(x)
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.show()
